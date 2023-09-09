783adee5db7858f6614f089b02bc6ef51afc2798
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:37 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -51,10 +51,35 @@ declare module 'react-native-ico-social-media' {
       'android' |
       'amazon';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -62,4 +87,3 @@ declare module 'react-native-ico-social-media' {
     export { iconNames };
     export default Icon;
 }
-
