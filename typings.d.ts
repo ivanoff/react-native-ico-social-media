ee19a7fa2815a3a56e7561dbcbc0c016a2fab9cf
Dimitry Ivanov <2@ivanoff.org.ua>
2023-11-04 23:49:18 +0200

    add colors change

--- typings.d.ts
+++ typings.d.ts
@@ -52,7 +52,73 @@ declare module 'react-native-ico-social-media' {
       'android' |
       'amazon';
 
+    type iconColors = '#00186A' |
+      '#002987' |
+      '#005DE1' |
+      '#0067FF' |
+      '#0074D2' |
+      '#00769D' |
+      '#0077B7' |
+      '#0085CC' |
+      '#0088C3' |
+      '#0090E1' |
+      '#00A4C3' |
+      '#00AAF2' |
+      '#00AD6C' |
+      '#00AEF0' |
+      '#00C967' |
+      '#00CE62' |
+      '#00D95F' |
+      '#00E470' |
+      '#00FD8D' |
+      '#01CB3E' |
+      '#04B086' |
+      '#050505' |
+      '#1587EA' |
+      '#171715' |
+      '#181A1C' |
+      '#222A30' |
+      '#2950A1' |
+      '#2A445D' |
+      '#333F40' |
+      '#475993' |
+      '#518FE1' |
+      '#69B4E5' |
+      '#7024C4' |
+      '#76A9EA' |
+      '#7AD06D' |
+      '#90A358' |
+      '#90B24C' |
+      '#9F1121' |
+      '#A6285F' |
+      '#AAC148' |
+      '#C21975' |
+      '#C74C4D' |
+      '#CF2217' |
+      '#D4D1D1' |
+      '#D64C1B' |
+      '#D80000' |
+      '#DC0000' |
+      '#DE9329' |
+      '#E09B3D' |
+      '#E65300' |
+      '#EB4427' |
+      '#F34A38' |
+      '#F61C0D' |
+      '#FC490B' |
+      '#FF007E' |
+      '#FF1A03' |
+      '#FF4175' |
+      '#FF4403' |
+      '#FF4B30' |
+      '#FF9414' |
+      '#FFAC35' |
+      '#FFBA00' |
+      '#FFFFFF';
+
     type backgroundType = 'circle' | 'rect' | 'button';
+
+    type colorsType = Record<iconColors, string>;
 
     type positionType = 'top' | 'bottom' | 'right' | 'left' |
       'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
@@ -69,6 +135,7 @@ declare module 'react-native-ico-social-media' {
       fontSize?: string;
       position?: positionType;
       color?: string;
+      colors: colorsType;
       backgroundColor?: string;
       radius?: number;
     };
@@ -78,6 +145,7 @@ declare module 'react-native-ico-social-media' {
       width?: number;
       height?: number;
       color?: string;
+      colors: colorsType;
       stroke?: number;
       background?: backgroundType | backgroundTypeObj;
       badge?: number | string | badgeTypeObj;
